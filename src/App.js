81519c63257dc3bac30881b23ecddcc45493a263
kelue <austincmoon5@gmail.com>
2023-02-19 09:30:35 +0100

    finish setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+       <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog/:id' element={< Blog />} />
+      </Routes>
+    </Container>
   );
 }
 
